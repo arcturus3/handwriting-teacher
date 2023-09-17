b72fa8b64db300c5a5e82aa63800b2a3b698686c
eharris733 <elliotmharris@gmail.com>
2023-09-16 21:27:48 -0400

    not finished but idea
    trying to get submitting to api working

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+
+
+
 let drawing = false;
+let imageUploaded = false;
 let cWidth = document.getElementById("canvasHolder").clientWidth;
 let cHeight = document.getElementById("canvasHolder").clientHeight;
 let canvas;
@@ -11,10 +15,13 @@ function setup() {
 
 function draw() {
   fill(0);
-  if (drawing) {
+  if (drawing && !imageUploaded) {
     stroke(0); // Set the stroke color (black)
     strokeWeight(7); // Set the stroke thickness
     line(pmouseX, pmouseY, mouseX, mouseY); // Draw a line from the previous mouse position to the current position
+  }
+  else if(imageUploaded){
+    console.log("we should display an image");
   }
 }
 
@@ -26,61 +33,70 @@ function mouseReleased() {
   drawing = false;
 }
 
-function submit(){
+function submit() {
 
-    let canvasElement = document.getElementById("defaultCanvas0");
-    let dataURL = canvasElement.toDataURL('image/jpeg');
 
-    // Create a link element to download the image
-    let a = document.createElement('a');
-    a.href = dataURL;
-    a.download = 'your_image.jpg'; // Specify the filename
+ 
 
-    // Trigger a click event to download the image
-    a.click();
-    background(220);
+  let canvasElement = document.getElementById("defaultCanvas0");
+  let dataURL = canvasElement.toDataURL("image/jpeg");
+
+  // Create a link element to download the image
+  let a = document.createElement("a");
+  a.href = dataURL;
+  a.download = "your_image.jpg"; // Specify the filename
+  
+  
+  fetch("/submit_canvas", {
+    method: "POST",
+    body: JSON.stringify({
+      canvas: ,
+      sample: "Elliot",
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  });
+
+  // Trigger a click event to download the image
+  a.click();
+  clear();
+  imageUploaded = false;
+  background(220);
 }
 
+function uploadImage() {
+  imageUploaded = true;
+  const fileInput = document.getElementById("fileInput");
+  const uploadButton = document.getElementById("uploadButton");
 
-// function submit() {
-//   let img = get(0, 0, cWidth, cHeight);
-//   img.loadPixels();
+  // Create a link element to download the image
+  uploadButton.addEventListener("click", () => {
+    fileInput.click(); // Trigger the file input when the button is clicked
+  });
+  fileInput.addEventListener("change", () => {
+    const selectedFile = fileInput.files[0];
+    if (selectedFile) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            // Draw the image on the canvas
+            const canvas = document.getElementById("defaultCanvas0");
+            const context = canvas.getContext("2d");
 
-//   // Get the pixel data from the captured image
-//   let pixels = img.pixels;
+            const img = new Image();
+            img.onload = () => {
 
-//   //mr.chat code
-//   // Create a 2D array to store black and white pixel values
-//   let bwPixels = [];
+            
+            //keep the aspect ratio reasonable
+            newHeight = cHeight;
+            newWidth = (img.width * cHeight) / img.height;
 
-//   // Iterate through the pixel data
-//   for (let y = 0; y < cHeight; y++) {
-//     let row = [];
-//     for (let x = 0; x < cWidth; x++) {
-//       // Calculate the pixel index in the pixel data array
-//       let pixelIndex = (y * cWidth + x) * 4; // Each pixel has 4 values (R, G, B, and A)
-
-//       // Get the R, G, and B values of the pixel
-//       let r = pixels[pixelIndex];
-//       let g = pixels[pixelIndex + 1];
-//       let b = pixels[pixelIndex + 2];
-
-//       // Convert the pixel to grayscale (average of R, G, and B values)
-//       let grayscaleValue = (r + g + b) / 3;
-
-//       // Push the grayscale value to the row array
-//       row.push(grayscaleValue);
-//     }
-//     // Push the row to the 2D array
-//     bwPixels.push(row);
-//   }
-//   let jsonData = {
-//     prompt: "Elliot",
-//     sample: bwPixels,
-//   };
-
-//   console.log(JSON.stringify(jsonData));
-
-//   // Clear the canvas after processing the pixels
-//   background(220);
-// }
+                context.drawImage(img, (cWidth - newWidth) / 2, 0, newWidth, newHeight);
+                canvas.style.display = "block";
+            };
+            img.src = e.target.result;
+        };
+        reader.readAsDataURL(selectedFile);
+    }
+});
+}
